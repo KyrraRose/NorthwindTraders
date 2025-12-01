236b814d1ea21e8bb06b4e251adf2fc2f87eee99
KyrraRose <krbosland1@gmail.com>
2025-12-01 14:18:13 -0500

    prepared statement

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -6,40 +6,38 @@ public class App {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // load the MySQL Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
-
-        // 1. open a connection to the database
-        // use the database URL to point to the correct database
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/northwind",
-                "root",
-                "yearup");
-
-
-        // create statement
-        // the statement is tied to the open connection
-        Statement statement = connection.createStatement();
-        // define your query
-        //product id
-        //- product name
-        //- unit price
-        //- units in stock
-
         String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
-        // 2. Execute your query
-        ResultSet results = statement.executeQuery(query);
-        // process the results
-        System.out.println("Id   Name                                Price   Stock");
-        System.out.println("---- ----------------------------------- ------- ------");
-        while (results.next()) {
-            int productID = results.getInt("ProductID");
-            String name = results.getString("ProductName");
-            double price = results.getDouble("UnitPrice");
-            int stock = results.getInt("UnitsInStock");
-            System.out.printf("%-4d %-35s $%-7.2f %-6d%n",productID,name,price,stock);
-        }
 
-        // 3. Close the connection
-        connection.close();
+        try {
+
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/northwind",
+                    "root",
+                    "yearup");
+
+            PreparedStatement statement = connection.prepareStatement(query);
+
+
+            // 2. Execute your query
+            ResultSet results = statement.executeQuery(query);
+            // process the results
+            System.out.println("Id   Name                                Price   Stock");
+            System.out.println("---- ----------------------------------- ------- ------");
+            while (results.next()) {
+                int productID = results.getInt("ProductID");
+                String name = results.getString("ProductName");
+                double price = results.getDouble("UnitPrice");
+                int stock = results.getInt("UnitsInStock");
+                System.out.printf("%-4d %-35s $%-7.2f %-6d%n", productID, name, price, stock);
+            }
+
+            results.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
 
 
     }
