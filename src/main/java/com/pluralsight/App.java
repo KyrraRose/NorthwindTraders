2519775d7e8835b5f88604a8cbf85089b66cd849
KyrraRose <krbosland1@gmail.com>
2025-12-01 14:01:21 -0500

    Displaying all products

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -19,13 +19,23 @@ public class App {
         // the statement is tied to the open connection
         Statement statement = connection.createStatement();
         // define your query
-        String query = "SELECT CompanyName FROM Customers";
+        //product id
+        //- product name
+        //- unit price
+        //- units in stock
+
+        String query = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products";
         // 2. Execute your query
         ResultSet results = statement.executeQuery(query);
         // process the results
+        System.out.println("Id   Name                                Price   Stock");
+        System.out.println("---- ----------------------------------- ------- ------");
         while (results.next()) {
-            String name = results.getString("CompanyName");
-            System.out.println(name);
+            int productID = results.getInt("ProductID");
+            String name = results.getString("ProductName");
+            double price = results.getDouble("UnitPrice");
+            int stock = results.getInt("UnitsInStock");
+            System.out.printf("%-4d %-35s $%-7.2f %-6d%n",productID,name,price,stock);
         }
 
         // 3. Close the connection
